2530285a7c142320eec6d39ad7419995cdf5d4c6
ShreyasDasari <dasari.sh@northeastern.edu>
2023-03-25 12:11:04 -0400

    Added all the files

---
+++ Assignment_8/app/models/sample.js
@@ -0,0 +1,10 @@
+// app/models/sample.js
+// grab the mongoose module
+var mongoose = require('mongoose');
+
+// define our sample model
+// module.exports allows us to pass this to other files when it is called
+module.exports = mongoose.model('Sample', {
+    userEmail: { type: String, default: '' },
+    userPassword: { type: String, default: '' }
+});
